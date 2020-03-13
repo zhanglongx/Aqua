33daac5c5c1867f4c16e29a33fc586d1d49c4cb7
Longxiao Zhang <zhanglongx@gmail.com>
2020-03-13 20:04:59 +0800

    pipe: re-write

--- driver/pipe.go
+++ driver/pipe.go
@@ -10,65 +10,153 @@ import (
 	"net"
 )
 
-// Video Codec
-const (
-	VideoH264 = iota
-	VideoHEVC
-)
+// Node alloc Pipe
+type Node struct {
+	// IP is the Svr IP
+	IP net.IP
 
-// Audio Codec
-const (
-	AudioG711a = iota
-	AudioG711mu
-	AudioMPGA
-)
+	// Prefix to identity services
+	Prefix int
 
-// SDP shared between path
-type SDP struct {
-	CodecVideo int
-	CodecAudio int
-
-	PtVideo int
-	PtAudio int
+	all map[int]*pipe
 }
 
-// Pipe is pipline shared between workers
-type Pipe struct {
+// pipe is pipeline shared between workers
+type pipe struct {
+	inPorts []int
 
-	// InPort is the data upstream ports, encoder
-	// uses this as sendto port
-	InPort []int
+	outIP []net.IP
 
-	// OutPort is the data downstream ports, decoder
-	// uses this as recvfrom port
-	OutPort []int
+	outPorts [][]int
 
-	// OutIp is the data downstream IP
-	OutIP net.IP
+	inWorkers  Worker
+	outWorkers []Worker
+}
 
-	// SDP
-	SDP SDP
+// Session is src or dst for workers
+type Session struct {
+	IP net.IP
+
+	Ports []int
 }
 
 var (
-	errAllocPipeF = errors.New("Alloc Pipe failed")
+	errSetSessError  = errors.New("Set session failed")
+	errNodeBadInput  = errors.New("Bad input for node")
+	errPipeExists    = errors.New("Pipe already exists")
+	errPipeNotExists = errors.New("Pipe doesnot exists")
 )
 
-// allocIn allocs InPort for a Pipe
-func (pi *Pipe) allocIn(w Worker) {
-	s := GetWorkerSlot(w)
-	id := GetWorkerWorkerID(w)
-
-	pi.InPort[0] = 8000 + 64*s + id
-	pi.InPort[0] = pi.InPort[0] + 1
+// Create a svr
+func (sr *Node) Create() {
 }
 
-// allocOut allocs OutPort for a Pipe
-func (pi *Pipe) allocOut(w Worker) {
-	id := GetWorkerWorkerID(w)
+// AllocPull alloc one pull
+func (sr *Node) AllocPull(id int, w Worker) error {
+	var p *pipe
 
-	pi.InPort[0] = 8000 + id
-	pi.InPort[0] = pi.InPort[0] + 1
+	if p = sr.all[id]; p == nil {
+		p = &pipe{inPorts: []int{8000 + sr.Prefix + 2*id, 8000 + sr.Prefix + 2*id + 2}}
+		sr.all[id] = p
+	}
 
-	pi.OutIP = GetWorkerWorkerIP(w)
+	if w == nil || !IsWorkerDec(w) {
+		return errNodeBadInput
+	}
+
+	for _, exists := range p.outWorkers {
+		if exists == w {
+			return errPipeExists
+		}
+	}
+
+	p.outWorkers = append(p.outWorkers, w)
+
+	wid := GetWorkerWorkerID(w)
+
+	ses := Session{Ports: []int{8000 + 2*wid, 8000 + 2*wid + 2}}
+	if err := SetDecodeSes(w, &ses); err != nil {
+		return err
+	}
+
+	// TODO: start here
+
+	return nil
+}
+
+// FreePull free one pull
+func (sr *Node) FreePull(id int, w Worker) error {
+	var p *pipe
+	if p = sr.all[id]; p == nil {
+		return errPipeNotExists
+	}
+
+	if w == nil || !IsWorkerDec(w) {
+		return errNodeBadInput
+	}
+
+	var exists Worker
+	var k int
+	for k, exists = range p.outWorkers {
+		if exists == w {
+			p.outWorkers[k] = nil
+			break
+		}
+	}
+
+	if exists != w {
+		return errPipeNotExists
+	}
+
+	// ip := GetWorkerWorkerIP(w)
+	// wid := GetWorkerWorkerID(w)
+
+	// ports := []int{8000 + 2*wid, 8000 + 2*wid + 2}
+
+	// TODO: free here
+
+	return nil
+}
+
+// AllocPush return Pipe
+func (sr *Node) AllocPush(id int, w Worker) error {
+	var p *pipe
+
+	if p = sr.all[id]; p == nil {
+		p = &pipe{inPorts: []int{8000 + sr.Prefix + 4*id, 8000 + sr.Prefix + 4*id + 2}}
+		sr.all[id] = p
+	}
+
+	if w == nil || !IsWorkerEnc(w) {
+		return errNodeBadInput
+	}
+
+	if exists := p.inWorkers; exists != nil {
+		// TODO re-do
+	}
+
+	ses := Session{IP: sr.IP,
+		Ports: p.inPorts}
+
+	if err := SetEncodeSes(w, &ses); err != nil {
+		return errSetSessError
+	}
+
+	p.inWorkers = w
+
+	return nil
+}
+
+// FreePush free one push
+func (sr *Node) FreePush(id int, w Worker) error {
+	var p *pipe
+	if p = sr.all[id]; p == nil {
+		return errPipeNotExists
+	}
+
+	if w == nil || !IsWorkerEnc(w) {
+		return errNodeBadInput
+	}
+
+	return nil
 }
